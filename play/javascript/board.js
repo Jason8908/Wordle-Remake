7c024fefa64ae7b664b98734aa9adf2667ef82bb
Jason <jasonsu894@gmail.com>
2022-05-19 20:49:15 -0400

    Finished main game functionality.

--- play/javascript/board.js
+++ play/javascript/board.js
@@ -1,28 +1,35 @@
 class Board {
-    constructor(width, height, box_size = 65) {
+    constructor(width, height, word, box_size = 65) {
         // Setting width and height of the board.
         this.width = width; // Corresponds to the number of characters in the word.
         this.height = height; // Corresponds to the number of guesses the user gets.
         this.box_size = box_size; // The size of each letter box.
+        this.word = word; // The correct word.
+        // Colours.
+        this.green = "#538D4E";
+        this.grey = "#3A3A3C";
+        this.yellow = "#B59F3B";
     }
     init() {
         // Variables to keep track of the state of the board.
         this.curr_y = 0;
         this.curr_x = 0;
+        this.active = true;
+        this.board_class = "board";
         // Creating the board, which is a table.
         const board = document.createElement('table');
         // Creating an array to store table values.
         this.board_arr = new Array();
         // Giving it a class so we can identify it later.
-        board.classList.add("board");
+        board.classList.add(this.board_class);
         // Adding the cells to the board.
-        for (let i = 0; i <= this.height; i++) {
+        for (let i = 0; i < this.height; i++) {
             // Creating a row for the board.
             const row = document.createElement('tr');
             row.classList.add('row');
             // Array representation.
             let row_arr = new Array();
-            for (let j = 0; j <= this.width; j++) {
+            for (let j = 0; j < this.width; j++) {
                 // Adding the individual cells to the table element.
                 const cell = document.createElement('td');
                 cell.classList.add('cell');
@@ -42,5 +49,114 @@ class Board {
         // Writing the board to the screen.
         const game = document.getElementById("game");
         game.appendChild(board);
+    }
+    endGame(guessed) {
+        this.active = false;
+        // If the player was able to guess the correct word.
+        if (guessed) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    // Input-related functions.
+    submit() {
+        // If the row is not full.
+        if (this.curr_x < this.width) return;
+        // Otherwise, check for correctness, and move on to next row.
+        if (this.checkRow()) return this.endGame(true);
+        // Move onto the next row and check for last guess used.
+        if (++this.curr_y == this.height) return this.endGame(false);
+        this.curr_x = 0;
+    }
+    backspace() {
+        // If the current row is empty.
+        if (this.curr_x < 1) return;
+        // Otherwise, delete the current letter from the board array.
+        this.curr_x--;
+        this.board_arr[this.curr_y][this.curr_x] = '';
+        // Update the board.
+        this.update(this.curr_x, this.curr_y);
+    }
+    letterIn(letter) {
+        // If the current row is full.
+        if (this.curr_x >= this.width) return;
+        // Otherwise, add a new letter!
+        letter = letter.toUpperCase();
+        this.board_arr[this.curr_y][this.curr_x] = letter;
+        // Updating the board.
+        this.update(this.curr_x, this.curr_y);
+        // Traversing forward.
+        this.curr_x++;
+    }
+    getColours() {
+        // An array to store the colour codes.
+        let colours = new Array(this.width);
+        // Turning the correct word into an array.
+        let solution = this.word.split('');
+        // Green pass.
+        for (let i = 0; i < this.width; i++) {
+            // Correct letter, correct position.
+            if (this.board_arr[this.curr_y][i] == solution[i]) {
+                colours[i] = this.green;
+                solution[i] = '';
+            }
+        }
+        // Check all letter combinations.
+        for (let i = 0; i < this.width; i++) {
+            for (let j = 0; j < this.word.length; j++) {
+                // Checking the letters.
+                if (this.board_arr[this.curr_y][i] == solution[j]) {
+                    // Correct letter, incorrect position.
+                    colours[i] = this.yellow;
+                    solution[j] = '';
+                    break;
+                }
+            }
+            if (!colours[i]) colours[i] = this.grey;
+        }
+        // Returning the colours array.
+        return colours;
+    }
+    drawRow(colours) {
+        // Looping through the row and assigning background colours.
+        for (let i = 0; i < this.width; i++) {
+            // Getting the current cell.
+            let cell = document.getElementById(`${this.curr_y}${i}`);
+            // Setting the colour.
+            cell.style.backgroundColor = colours[i];
+            cell.style.color = "white";
+            cell.style.border = "none";
+        }
+    }
+    checkRow() {
+        // Joining the input array into a string.
+        let guess = this.board_arr[this.curr_y].join("");
+        // Generating an array of colours to assign to the row.
+        this.drawRow(this.getColours()); // Applying the colour change.
+        // If the guess is right.
+        if (guess == this.word) return true;
+        return false;
+    }
+    // Board updates.
+    update(x, y) {
+        // Update the board based on the array element.
+        let cell = document.getElementById(`${y}${x}`);
+        // If we want to add a letter to the cell.
+        if (this.board_arr[y][x]) {
+            // Creating a new letter element.
+            let letter = document.createElement('p');
+            letter.innerHTML = this.board_arr[y][x];
+            letter.classList.add('letter');
+            // Appending the new letter element.
+            cell.appendChild(letter);
+            cell.style.color = 'black';
+        }
+        // If we want to empty the cell.
+        else {
+            cell.innerHTML = this.board_arr[y][x];
+            cell.style.color = 'grey';
+        }
     }
 }
--- play/javascript/driver.js
+++ play/javascript/driver.js
@@ -1,18 +1,29 @@
+window.onload = init;
 // Settings:
 const num_of_guesses = 6;
 const word_len = 5;
+// Keypress functions.
+function validKey(event) {
+    // Detecting if the inputted key is a valid input.
+    let my_key = event.key.toUpperCase();
+    let ascii = my_key.charCodeAt(0);
+    // If the key pressed is not valid, exit the function.
+    if (my_key != 'ENTER' && my_key != 'BACKSPACE' && my_key.length > 1) return false;
+    if (ascii < 65 || ascii > 90) return false;
+    return true;
+}
 // Initialization Function.
 function init() {
     // Instantiating a new board object.
-    const board = new Board(word_len, num_of_guesses);
+    const board = new Board(word_len, num_of_guesses, "PENIS");
     // Initializing the board.
     board.init();
-    console.log(board.board_arr);
     // Keyboard listener.
-    document.addEventListener('keypress', (event) => {
-        // Detecting if the inputted key is a valid input.
-        let ascii = event.key.charCodeAt(0);
-        if (event.key)
-    })
+    document.addEventListener('keydown', (event) => {
+        if (!board.active || !validKey(event)) return false;
+        // Deciding what to do based on keypress.
+        if (event.key == 'Enter') board.submit(); // Enter.
+        else if (event.key == 'Backspace') board.backspace(); // Backspace.
+        else board.letterIn(event.key); // General letter.
+    });
 }
-window.onload = init;
